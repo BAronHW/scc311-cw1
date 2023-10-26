760563381b08db4ecbe99c603d9d6b11c7b11cd8
Aaron Wan <aaronwan808@gmail.com>
2023-10-26 21:25:22 +0100

    fuck you liu zi

--- real_cw/Server/Server.java
+++ real_cw/Server/Server.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
@@ -22,6 +24,7 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SealedObject;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Server extends UnicastRemoteObject implements Auction {
     private HashMap<Integer, SealedObject> itemMap = new HashMap<>();
@@ -29,41 +32,56 @@ public class Server extends UnicastRemoteObject implements Auction {
 
     public Server() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, IOException {
         super();
-        AuctionItem expensiveitem = new AuctionItem();
-        expensiveitem.itemID = 1;
-        expensiveitem.name = "expensive";
-        expensiveitem.description = "its over 9000";
-        expensiveitem.highestBid = 700;
-
-        AuctionItem cheapItem = new AuctionItem();
-        cheapItem.itemID = 30;
-        cheapItem.name = "cheap";
-        cheapItem.description = "its less than 9000";
-        cheapItem.highestBid = 70;
         
 
-        AuctionItem middleitem = new AuctionItem();
-        middleitem.itemID = 40;
-        middleitem.name = "ok";
-        middleitem.description = "meh";
-        middleitem.highestBid = 100;
-
-            
-            KeyGenerator keygen = KeyGenerator.getInstance("AES");
-            keygen.init(128);
-            secretKey = keygen.generateKey();
-            String encodedkey = Base64.getEncoder().encodeToString(secretKey.getEncoded());
-            Cipher c = Cipher.getInstance("AES");
-            c.init(Cipher.ENCRYPT_MODE,secretKey);
             String dirpath = new File(System.getProperty("user.dir")).getParent()+"/keys";
             String filename = "testKey.aes";
             String filepath = Paths.get(dirpath,filename).toString();
-            try(PrintWriter out = new PrintWriter((filepath))){
-                out.println(encodedkey);
+            File keyfile = new File(filepath);
 
-            }catch(IOException e){
+            if(!keyfile.exists()||keyfile.length()==0){
+                KeyGenerator keygen = KeyGenerator.getInstance("AES");
+                keygen.init(128);
+                secretKey = keygen.generateKey();
+                String encodedkey = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+                Cipher c = Cipher.getInstance("AES");
+                c.init(Cipher.ENCRYPT_MODE,secretKey);
+                try(PrintWriter out = new PrintWriter((filepath))){
+                    out.println(encodedkey);
+
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }else{
+                try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+                String encodedKey = br.readLine();
+                byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
+                secretKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+            } catch (IOException e) {
                 e.printStackTrace();
             }
+            }
+            AuctionItem expensiveitem = new AuctionItem();
+            expensiveitem.itemID = 1;
+            expensiveitem.name = "expensive";
+            expensiveitem.description = "its over 9000";
+            expensiveitem.highestBid = 700;
+
+            AuctionItem cheapItem = new AuctionItem();
+            cheapItem.itemID = 30;
+            cheapItem.name = "cheap";
+            cheapItem.description = "its less than 9000";
+            cheapItem.highestBid = 70;
+            
+
+            AuctionItem middleitem = new AuctionItem();
+            middleitem.itemID = 40;
+            middleitem.name = "ok";
+            middleitem.description = "meh";
+            middleitem.highestBid = 100;
+            
+            Cipher c = Cipher.getInstance("AES");
+            c.init(Cipher.ENCRYPT_MODE,secretKey);
             SealedObject sealedexitem = new SealedObject(expensiveitem, c);
             SealedObject sealedcheapitem = new SealedObject(cheapItem, c);
             SealedObject sealedmiditem = new SealedObject(middleitem,c);
